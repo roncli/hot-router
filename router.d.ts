e4fd85de070789434e04b13d8aa25b31d000197b
Ronald M. Clifford <roncli@roncli.com>
2021-02-25 14:55:02 -0800

    Add typings for new error function.

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 import {EventEmitter} from "events"
-import {Router as ExpressRouter} from "express"
+import {Router as ExpressRouter, NextFunction} from "express"
 
 declare class Router extends EventEmitter {
     addListener(event: "error", listener: (arg: {message: string, err: Error, req: Express.Request}) => void): this
@@ -14,6 +14,16 @@ declare class Router extends EventEmitter {
      * @returns {Promise<ExpressRouter>} A promise that resolves with the router to use for the website.
      */
     getRouter(routesPath: string, options?: {hot?: boolean}): Promise<ExpressRouter>
+
+    /**
+     * Handles a router error.
+     * @param {Error} err The error object.
+     * @param {Express.Request} req The request.
+     * @param {Express.Response} res The response.
+     * @param {NextFunction} next The function to be called if the error is not handled.
+     * @returns {Promise} A promise that resolves when the error is handled.
+     */
+    error(err: Error, req: Express.Request, res: Express.Response, next: NextFunction): Promise<void>
 }
 
 export = Router
